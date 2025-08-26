a901f05dd570afcb5d75d3d07c1ef5fe0749216a
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2025-08-26 11:19:37 +0530

    feat: add Tailwind CSS configuration and new component aliases; update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,33 @@ module.exports = {
       fontFamily: {
         'figtree': ['Figtree', 'ui-sans-serif', 'system-ui', 'sans-serif'],
         'sans': ['Figtree', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        'comfortaa': ['Comfortaa', 'ui-rounded', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#f0f9f3',
+          100: '#dcefe2',
+          200: '#bde0ca',
+          300: '#90c9a8',
+          400: '#5fad81',
+          500: '#4ea674',  // Main green
+          600: '#459071',  // Dark green
+          700: '#2d664c',
+          800: '#26523d',
+          900: '#214334',
+        },
+        secondary: {
+          50: '#f0f9f3',
+          100: '#dcefe2',
+          200: '#bde0ca',
+          300: '#90c9a8',
+          400: '#5fad81',
+          500: '#4ea674',
+          600: '#459071',
+          700: '#2d664c',
+          800: '#26523d',
+          900: '#214334',
+        },
       },
     },
   },
